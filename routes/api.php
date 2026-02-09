4c502de2fb8be7eb763c89c6be206fcdfa824698
Gustavo Ramirez <tavo.joy.36@gmail.com>
2026-02-09 11:17:58 -0400

    rutas faltantes

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,7 @@ use App\Http\Controllers\Api\ProductController;
 // })->middleware('auth:sanctum');
 
 
-Route::post('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::post('products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
