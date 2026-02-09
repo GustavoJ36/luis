d356e73be7ba3f07b06ca6f4e4bde30c8f86eb0f
Gustavo Ramirez <tavo.joy.36@gmail.com>
2026-02-09 10:40:09 -0400

    controller

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::post('products', 'ProductController@index')->name('products.index');
+
+Route::post('products', [ProductController::class, 'index'])->name('products.index');
